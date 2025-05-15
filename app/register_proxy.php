f1fe4c47f695b98a08fa06dcbe13cf921c149f39
FilipPorzucek <filipporzucek81@gmail.com>
2025-05-15 21:00:30 +0200

    php file

--- app/register_proxy.php
+++ app/register_proxy.php
@@ -1,29 +1,38 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $input = file_get_contents('php://input');
-
 $data = json_decode($input, true);
-if (!$data || !isset($data['email'], $data['password'], $data['date_of_birth'])) {
+
+if (!$data || !isset($data['name'], $data['email'], $data['password'], $data['dateOfBirth'])) {
     http_response_code(400);
     echo json_encode(['error' => 'Missing fields']);
     exit;
 }
 
-$ch = curl_init('http://127.0.0.1:8000/login'); 
+$ch = curl_init('http://127.0.0.1:8000/users'); 
 
 curl_setopt_array($ch, [
     CURLOPT_POST           => true,
     CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true, 
     CURLOPT_HTTPHEADER     => [
         'Content-Type: application/json',
     ],
     CURLOPT_POSTFIELDS     => json_encode($data),
 ]);
-
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 http_response_code($httpCode);
-header('Content-Type: application/json');
 echo $response;
+?>
